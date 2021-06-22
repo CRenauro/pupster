b1da3a5386d9dca22bf42a0b7dcb2a0bd224249b
CRenauro <crenauro@optimum.net>
2021-06-22 14:41:46 -0400

    filled out search page

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
+// Export an object containing methods we'll use for accessing the Dog.Ceo API
+
 export default {
-  getRandomDog: function () {
+  getRandomDog: function() {
     return axios.get("https://dog.ceo/api/breeds/image/random");
   },
-  getDogsOfBreed: function (breed) {
+  getDogsOfBreed: function(breed) {
     return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
   },
-  getBaseBreedsList: function () {
+  getBaseBreedsList: function() {
     return axios.get("https://dog.ceo/api/breeds/list");
-  },
+  }
 };
